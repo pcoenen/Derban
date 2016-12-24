57e337b1b7eac6c2e63d16ca3bca379e0324a484
Pieter-Jan <pieterjan.coenen@gmail.com>
2016-12-24 17:22:55 +0100

    fix already blocked

--- firewallD.py
+++ firewallD.py
@@ -3,9 +3,8 @@ import subprocess
 
 def block_ip(ip):
     output = subprocess.check_output("firewall-cmd --add-rich-rule=\"rule family='ipv4' source address='" + ip + "' reject\"", shell=True)
-    if(output == "success"):
+    if(output == "success" or output == "Warning: ALREADY_ENABLED"):
         return True
-    print output
     return False
 
 def deblock_ip(ip):
