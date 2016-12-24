76ee4a8a1513b0ee0fb001c98b2c77e7f1994035
Pieter-Jan <pieterjan.coenen@gmail.com>
2016-12-24 17:41:28 +0100

    more debug

--- firewallD.py
+++ firewallD.py
@@ -36,6 +36,7 @@ def remove_all_permanent():
     stream = os.popen("firewall-cmd --list-all")
     line = stream.readline()
     while line != "":
+        print line
         matchObj = re.match(r'rule family=\"ipv4\" source address=\"(.*)\" reject', line, re.M | re.I)
         if matchObj:
             ip = matchObj.group(1)
