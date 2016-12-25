142605bb2c07e6f371aad52f1c8cd50fd6994884
Pieter-Jan <pieterjan.coenen@gmail.com>
2016-12-25 15:52:09 +0100

    installer

--- installation/install.sh
+++ installation/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cp -r /source /etc/derban
-cp derban.sh /usr/bin/derban.sh
-cp derban.service /lib/systemd/system/derban.service
-ln -s /lib/systemd/system/derban.service /etc/systemd/system/derban.service
+cp -rf ../source /etc/derban
+cp -f derban.sh /usr/bin/derban.sh
+cp -f derban.service /lib/systemd/system/derban.service
+ln -s -f /lib/systemd/system/derban.service /etc/systemd/system/derban.service
 
 
+systemctl daemon-reload
 systemctl enable derban.service
 systemctl start derban.service
