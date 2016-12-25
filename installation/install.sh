fa74da8f63a25207a12ec7f39f87b30ae6a55ebf
Pieter-Jan <pieterjan.coenen@gmail.com>
2016-12-25 15:42:56 +0100

    installation update

--- installation/install.sh
+++ installation/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -r source /etc/derban
+cp -r /source /etc/derban
 cp derban.sh /usr/bin/derban.sh
 cp derban.service /lib/systemd/system/derban.service
 ln -s /lib/systemd/system/derban.service /etc/systemd/system/derban.service
